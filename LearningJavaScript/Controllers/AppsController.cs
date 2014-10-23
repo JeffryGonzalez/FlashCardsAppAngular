3ed677d1d91eb566b7a2bd54aa6ba0c3eeb8ac38
Jeffry Gonzalez <jeff@hypertheory.com>
2014-10-23 05:49:45 -0400

    new version

--- LearningJavaScript/Controllers/AppsController.cs
+++ LearningJavaScript/Controllers/AppsController.cs
@@ -6,46 +6,50 @@ using System.Web.Mvc;
 
 namespace LearningJavaScript.Controllers
 {
-    public class AppsController : Controller
-    {
-        public ActionResult SimpleCalculator()
-        {
-            return View();
-        }
+	public class AppsController : Controller
+	{
+		public ActionResult SimpleCalculator()
+		{
+			return View();
+		}
 
-        public ActionResult TemperatureConverter()
-        {
-            return View();
-        }
+		public ActionResult TemperatureConverter()
+		{
+			return View();
+		}
 
-        public ActionResult Demo1()
-        {
-            return View();
-        }
+		public ActionResult Demo1()
+		{
+			return View();
+		}
 
-        public ActionResult Employees()
-        {
-            return View();
-        }
+		public ActionResult Employees()
+		{
+			return View();
+		}
 
-        public ActionResult AngularDemo()
-        {
-            return View();
-        }
+		public ActionResult AngularDemo()
+		{
+			return View();
+		}
 
-        public ActionResult FlashCards()
-        {
-            return View();
-        }
+		public ActionResult FlashCards()
+		{
+			return View();
+		}
 
-        public ActionResult IndexedDbDemo()
-        {
-            return View();
-        }
+		public ActionResult FlashCardsNew()
+		{
+			return View();
+		}
+		public ActionResult IndexedDbDemo()
+		{
+			return View();
+		}
 
-        public FileResult Manifest(string fileName)
-        {
-            return File("/" + fileName, "text/cache-manifest");
-        }
-    }
+		public FileResult Manifest(string fileName)
+		{
+			return File("/" + fileName, "text/cache-manifest");
+		}
+	}
 }
